4e3bbbf3dbc0880ba9681fc13e9b2198c0138b56
Yizhuo Wu <yizhuw1@uci.edu>
2021-12-12 15:52:05 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -229,7 +229,7 @@ class Game():
 def main():
 
     playerAI = PlayerAI() # change this to PlayerAI() to test your player!
-    computerAI = EasyAI() # change this to a more sophisticated player you've coded
+    computerAI = MediumAI() # change this to a more sophisticated player you've coded
     displayer = Displayer()
     game = Game(playerAI = playerAI, computerAI = computerAI, N = 7, displayer=displayer)
     

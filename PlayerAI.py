b453bcc2b2cb9d08510440c92bd896e1f31b4d68
Yizhuo Wu <yizhuw1@uci.edu>
2021-12-12 20:46:49 -0500

    Updated PlayerAI.py

--- PlayerAI.py
+++ PlayerAI.py
@@ -48,6 +48,8 @@ class PlayerAI(BaseAI):
         depth = 3
         optimal_result = self.mini_max_get_move(grid, depth, alpha, beta, maximizing_player)
         optimal_state = optimal_result[0]
+        return optimal_state
+        #print(optimal_state.print_grid())
 
         #if not optimal_state:
         #    return random.choice(grid.getAvailableCells())
@@ -82,22 +84,43 @@ class PlayerAI(BaseAI):
         if not optimal_state:
             return random.choice(grid.getAvailableCells())
 
+        #print(optimal_result)
         rows, cols = optimal_state.dim, optimal_state.dim
         for i in range(rows):
             for j in range(cols):
                 if grid.map[i][j] != optimal_state.map[i][j]:
-                    return (i, j)
-        return None
+                    result = (i, j)
+                    return result
+
+        """
+        Very Rare Edge case, player is close to opponent and there is one place to move and
+        place trap, will cause exceptions:
+        x x x
+        x 1 x
+        x x
+        x x 2
+        """
+        ava_moves = grid.get_neighbors(grid.find(self.player_num), True)
+        if len(ava_moves) == 1 and (i, j) in ava_moves:
+            random_pos = random.choice(grid.getAvailableCells())
+            while random_pos == (i, j):
+                random_pos = random.choice(grid.getAvailableCells())
+            return random_pos
+
+        #return result
 
     def mini_max_get_move(self, grid, depth, alpha, beta, maximizing_player):
         player_pos = grid.find(self.player_num)
         player_valid_neighbors = grid.get_neighbors(player_pos, True)
+        #print("DEP = : ",depth)
+        #print(player_valid_neighbors)
 
+        '''
         if not player_valid_neighbors:
             eval_result = (grid, self.OCLS(grid, self.player_num))
             #print("no move: score: ",eval_result[1])
             return eval_result
-
+        '''
         if depth == 0:
             eval_result = (grid, self.OCLS(grid, self.player_num))
             #print("0: score: ",eval_result[1])
@@ -111,16 +134,18 @@ class PlayerAI(BaseAI):
 
                 # Recursion
                 eval_result = self.mini_max_get_move(grid_copy, depth - 1, alpha, beta, False)
-                eval_grid = eval_result[0]
+                #eval_grid = eval_result[0]
                 eval_score = eval_result[1]
                 if max_eval[1] < eval_score:
-                    max_eval = eval_result
+                    #max_eval = eval_result
+                    max_eval = (child, eval_score)
 
                 #Alpha Beta Pruning
                 if alpha < eval_score:
                     alpha = eval_score
                 if beta <= alpha:
                     break
+
             return max_eval
         else:
             min_eval = (None, float('inf'))
@@ -131,10 +156,12 @@ class PlayerAI(BaseAI):
 
                 # Recursion
                 eval_result = self.mini_max_get_move(grid_copy, depth - 1, alpha, beta, True)
-                eval_grid = eval_result[0]
+                #eval_grid = eval_result[0]
                 eval_score = eval_result[1]
                 if eval_score < min_eval[1]:
-                    min_eval = eval_result
+                    #min_eval = eval_result
+                    min_eval = (child, eval_score)
+
 
                 #Alpha Beta Pruning
                 if eval_score < beta:
@@ -149,18 +176,12 @@ class PlayerAI(BaseAI):
         opponent_valid_neighbors = grid.get_neighbors(opponent_pos, True)
 
         if not opponent_valid_neighbors:
-            #eval_result = (grid, self.IS(grid, 3 - self.player_num)) ------->IS
             eval_result = (grid, self.OCLS(grid, 3 - self.player_num))
             return eval_result
 
         # Base case, evaluate leaf nodes.
         if depth == 0:
-            #print("DEP = 0")
-            #grid.print_grid()
-            #print()
-            #eval_result = (grid, self.IS(grid, 3 - self.player_num)) -------->IS
             eval_result = (grid, self.OCLS(grid, 3 - self.player_num))
-            #print("Score: ", eval_result[1])
             return eval_result
 
         # find available positions for throwing traps
@@ -216,41 +237,28 @@ class PlayerAI(BaseAI):
 
             return min_eval
 
-    def IS(self, grid, player_num):
-
-        # find all available moves by Player
-        player_moves = grid.get_neighbors(grid.find(player_num), only_available = True)
-
-        # find all available moves by Opponent
-        opp_moves = grid.get_neighbors(grid.find(3 - player_num), only_available = True)
-
-        return len(player_moves) - len(opp_moves)
-
     '''
     The difference between the Player's sum of possible moves looking
     one step ahead and the Opponent's sum of possible moves looking one step ahead
-
-    def OCLS(self, grid, player_num):
-        sum = 0
-        available_moves = grid.get_neighbors(grid.find(player_num), only_available = True)
-        for potential_moves in available_moves:
-            ava_for_potential_moves = grid.get_neighbors(potential_moves, only_available = True)
-            sum += len(ava_for_potential_moves)
-
-        return sum
     '''
     def OCLS(self, grid, player_num):
-        sum = 0
+        player_set = set()
         available_moves = grid.get_neighbors(grid.find(player_num), only_available = True)
         for potential_moves in available_moves:
             ava_for_potential_moves = grid.get_neighbors(potential_moves, only_available = True)
-            sum += len(ava_for_potential_moves)
+            for move in ava_for_potential_moves:
+                if move not in player_set:
+                    player_set.add(move)
+        sum = len(player_set)
 
-        op_sum = 0
+        op_set = set()
         op_available_moves = grid.get_neighbors(grid.find(3 - player_num), only_available = True)
         for potential_moves in op_available_moves:
             ava_for_potential_moves = grid.get_neighbors(potential_moves, only_available = True)
-            op_sum += len(ava_for_potential_moves)
+            for move in ava_for_potential_moves:
+                if move not in op_set:
+                    op_set.add(move)
+        op_sum = len(op_set)
 
         return sum - op_sum
 
